20c172256238501560cf53bb0cb6530cd9076aab
alessioarcara <Spammer123@>
2024-12-04 07:19:08 +0100

    Refactor Accumulator e InstanceMatcher
    - Changed 2D accumulator to 3D one
    - Extracted some methods from 'match' method to improve clarity

--- Accumulator.py
+++ Accumulator.py
@@ -1,50 +1,54 @@
 import numpy as np
-import cv2 as cv
-
 
 class Accumulator:
-    def __init__(self, shape, quantization_step):
-        self.arr = np.zeros((shape[0] // quantization_step, shape[1] // quantization_step))
+    """
+    # 3D Accumulator with spatial dimensions matching the target image (quantized with a step) and depth equal the number of models
+    """
+    def __init__(self, target_img_shape, n_models, quantization_step):
         self.quantization_step = quantization_step
+        self.shape = (
+            target_img_shape[0] // quantization_step,
+            target_img_shape[1] // quantization_step, 
+            n_models
+        )
+        self.arr = np.zeros(self.shape)
         self.matches = {}
         
-    def cast_vote(self, pt, matching_pts):
+    def cast_vote(self, model_idx, pt, matching_pts):
         x, y = int(pt[0] / self.quantization_step), int(pt[1] / self.quantization_step)
-        if 0 <= x < self.arr.shape[1] and 0 <= y < self.arr.shape[0]:
-            self.arr[y, x] += 1
-            if (y, x) in self.matches:
-                self.matches[(y, x)].append(matching_pts)
-            else: 
-                self.matches[(y, x)] = [matching_pts]
-                
-    def check_votes(self):
-        return np.sum(self.arr) > 0
-    
-    def get_votes(self):
-        return self.arr[self.arr > 0]
-    
-    def get_matching_pts(self, th):
-        x_idxs, y_idxs = np.where(self.arr >= th)
-        
-        matching_pts = []
-        
-        for x, y in zip(x_idxs, y_idxs):
-            model_pts = [model_pt for model_pt, _ in self.matches[x, y]]
-            target_pts = [target_pt for _, target_pt in self.matches[x, y]]
-            matching_pts.append((model_pts, target_pts))
-            
+        if 0 <= y < self.shape[0] and 0 <= x < self.shape[1]:
+            self.arr[y, x, model_idx] += 1
+            self.matches.setdefault((y, x, model_idx), []).append(matching_pts)
+
+    def get_matching_pts(self, th=1):
+        y_idxs, x_idxs = np.where(self.arr >= th)
+        matching_pts = [
+        (
+            self.arr[y, x],  # Numero di voti nel punto (y, x).
+            (
+                [model_pt for model_pt, _ in self.matches[(y, x)]],
+                [target_pt for _, target_pt in self.matches[(y, x)]],
+            )
+        )
+            for y, x in zip(y_idxs, x_idxs)
+        ]
         return matching_pts
 
-    def nms(self, size=3, min_votes=4):
+    def nms_3d(self, min_votes, size=3):
+        h, w, _ = self.shape
         b = size // 2
-        padded_arr = cv.copyMakeBorder(self.arr, b, b, b, b, cv.BORDER_CONSTANT, value=0)
-        nms_arr = np.zeros(self.arr.shape)
         
-        for i in range(b, self.arr.shape[0] + b):
-            for j in range(b, self.arr.shape[1] + b):
-                val = padded_arr[i, j]
-                window = padded_arr[i - b:i + b + 1, j - b:j + b + 1]
-                if val == np.max(window) and val >= min_votes:
-                    nms_arr[i - b, j - b] = val
-                    
-        self.arr = nms_arr
+        padded_arrays = np.pad(self.arr, pad_width=((0, 0), (b, b), (b, b)), mode='constant', constant_values=0)
+        nms_result = np.zeros((h, w))
+
+        for y in range(h):
+            for x in range(w):
+                window = padded_arrays[:, y:y + size, x:x + size]
+                central_values = padded_arrays[:, y + b, x + b]
+
+                max_value = np.max(window)
+
+                if np.any(central_values == max_value) and max_value >= min_votes:
+                    nms_result[y, x] = max_value
+
+        return nms_result
--- InstanceMatcher.py
+++ InstanceMatcher.py
@@ -4,68 +4,110 @@ import matplotlib.pyplot as plt
 import glob
 from Accumulator import Accumulator
 
-LOWE_THRESHOLD = 0.7
-
 
 class Feature:
-    def __init__(self, kp, des):
+    def __init__(self, kp):
         self.pt = kp.pt
         self.angle = kp.angle 
         self.scale = kp.size
         
     
 class StarModel:
-    def __init__(self, features, descriptors):
+    def __init__(
+            self, 
+            features, 
+            descriptors, 
+            model_img_path, 
+            model_shape
+        ):
         self.features = features
         self.descriptors = descriptors
+        self.model_img_path = model_img_path 
+        self.model_shape = model_shape
         
-        x = 0
-        y = 0
-        n = len(features)
-        
-        for f in features:
-            x += f.pt[0]
-            y += f.pt[1]
+        pts = np.array([f.pt for f in features])
+        self.barycenter = np.mean(pts, axis=0)
             
-        self.barycenter = (x/n, y/n)
-        self.joining_vectors = [(self.barycenter[0] - f.pt[0], self.barycenter[1] - f.pt[1]) for f in features]
-        
+        self.joining_vectors = [
+            (self.barycenter[0] - f.pt[0], self.barycenter[1] - f.pt[1]) 
+            for f in features
+        ]
+
 
 class BoundingBox:
     """
     pt1: The top-left point.
     pt2: The bottom-right point.
     """
-    def __init__(self, pts):
+    def __init__(self, pts, model_name, confidence):
         self.pt1 = tuple(map(int, pts[0]))
         self.pt2 = tuple(map(int, pts[1]))
+        self.model_name = model_name
+        self.confidence = confidence
         
-    def get_pos(self):
+    def center(self):
         return (self.pt1[0] + self.pt2[0]) / 2, (self.pt1[1] + self.pt2[1]) / 2
     
-    def get_width(self):
+    def width(self):
         return self.pt2[0] - self.pt1[0]
         
-    def get_height(self):
+    def height(self):
         return self.pt2[1] - self.pt1[1]
     
     def get_pts(self):
         return self.pt1, self.pt2
+    
+    def intersects(self, other):
+        x_overlap = (self.pt1[0] < other.pt2[0]) and (self.pt2[0] > other.pt1[0])
+        y_overlap = (self.pt1[1] < other.pt2[1]) and (self.pt2[1] > other.pt1[1])
+        return x_overlap and y_overlap 
+
+    @staticmethod
+    def group_overlapping_boxes(bboxes):
+        groups = []
+        while bboxes:
+            curr_bbox = bboxes.pop(0)
+            group = [curr_bbox]
+
+            for box in bboxes[:]:
+                if curr_bbox.intersects(box):
+                    group.append(box)
+                    bboxes.remove(box)
+            groups.append(group)
+
+        return groups
 
         
 class InstanceMatcher:
-    def __init__(self, model_folder, preprocessing_steps=[]):
-        self.sift = cv.SIFT_create()
-        self.models = self.load_models(model_folder)
-        self.preprocessing_steps = preprocessing_steps
+    LOWE_THRESHOLD  = 0.75
+
+    def __init__(
+        self, 
+        model_folder, 
+        preprocessing_steps=None, 
+        feature_detector=None,
+        show_models = False 
+    ):
+        self.preprocessing_steps = preprocessing_steps or []
+        self.feature_detector = feature_detector or cv.SIFT_create()
+        self.models = self._load_models(model_folder)
+        if show_models:
+            pass
+        print(f"Loaded {len(self.models)} models from {model_folder}")
         
-    def load_models(self, model_folder):
+    def _load_models(self, model_folder):
+        """
+        GHT Offline Phase 
+        """
         models = []
         for filename in glob.glob(f"{model_folder}/*.png"):
             model_img = cv.imread(filename)
-            # OFFLINE PHASE
-            features, descriptors = self.compute_features(model_img)
-            models.append((StarModel(features, descriptors), filename, model_img.shape[:2]))
+            if model_img is None:
+                print(f"Couldn't read image: {filename}")
+                continue
+
+            features, descriptors = self._compute_features(model_img)
+            models.append(StarModel(features, descriptors, filename, model_img.shape[:2]))
         return models
             
     def preprocess(self, img):
@@ -73,32 +115,70 @@ class InstanceMatcher:
             img = preprocess(img)
         return img
     
-    def compute_features(self, img):
-        keypoints, descriptors = self.sift.detectAndCompute(img, None)
-        features = [Feature(kp, des) for kp, des, in zip(keypoints, descriptors)]
+    def _compute_features(self, img):
+        keypoints, descriptors = self.feature_detector.detectAndCompute(img, None)
+        features = [Feature(kp) for kp in keypoints]
         return features, descriptors
 
-    def match(self, target_img, quantization_step, th=None, show_accumulator=False):
-        target_img = self.preprocess(cv.cvtColor(cv.imread(target_img), cv.COLOR_BGR2RGB))
-        # ONLINE PHASE
-        target_features, target_descriptors = self.compute_features(target_img)
+    def _nms_bboxes(self, bboxes):
+        print(len(bboxes))
+        groups = BoundingBox.group_overlapping_boxes(bboxes.copy())
 
-        found = []
-        for (model, filename, model_img_shape) in self.models:
+        non_overlapping_boxes = []
+        for group in groups:
+            group.sort(key=lambda bbox: bbox.confidence, reverse=True)
+            non_overlapping_boxes.append(group[0])
+        return non_overlapping_boxes 
+
+    def _extract_bboxes(self, found): 
+        bboxes = []
+
+        for aa, model_img_shape in found:
+            for vote_count, (model_pts, target_pts) in aa.get_matching_pts():
+
+                model_pts = np.float32(model_pts).reshape((-1,1,2))       
+                target_pts = np.float32(target_pts).reshape((-1,1,2))
+
+                H, _ = cv.findHomography(model_pts, target_pts, cv.RANSAC, 5.0)
+                
+                pts = np.float32([[0, 0],[model_img_shape[1]-1, model_img_shape[0]-1]]).reshape(-1,1,2)
+                transformed_pts = cv.perspectiveTransform(pts, H).reshape(-1, 2)
+                
+                bboxes.append(BoundingBox(transformed_pts, aa.model_name, vote_count))
+
+        return bboxes
+    
+    def match(
+        self, 
+        target_img_path, 
+        quantization_step, 
+        th=4, 
+        show_accumulator=False
+    ):
+        """
+        GHT Online Phase 
+        """
+        target_img = self.preprocess(cv.cvtColor(cv.imread(target_img_path), cv.COLOR_BGR2RGB))
+        target_features, target_descriptors = self._compute_features(target_img)
+
+        aa = Accumulator(target_img.shape, len(self.models), quantization_step)
+
+        for i, model in enumerate(self.models):
             matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
-            # queryIdx -> keypoints1
-            # trainIdx -> keypoints2
             matches = matcher.knnMatch(target_descriptors, model.descriptors, k=2)
-            good_matches = [m for m, n in matches if m.distance < LOWE_THRESHOLD * n.distance]
+
+            good_matches = [m for m, n in matches if m.distance < self.LOWE_THRESHOLD * n.distance]
             
-            aa = Accumulator((target_img.shape[0] , target_img.shape[1]), quantization_step)
             for m in good_matches:
-                p_j = target_features[m.queryIdx].pt
-                p_i = model.features[m.trainIdx].pt
+                target_feature = target_features[m.queryIdx]
+                model_feature = model.features[m.trainIdx]
+
+                p_j = target_feature.pt
+                p_i = model_feature.pt
                 v_i = model.joining_vectors[m.trainIdx]
 
-                ds = target_features[m.queryIdx].scale / model.features[m.trainIdx].scale
-                da = (target_features[m.queryIdx].angle - model.features[m.trainIdx].angle) % 360
+                ds = target_feature.scale / model_feature.scale
+                da = (target_feature.angle - model_feature.angle) % 360
 
                 theta = np.radians(da)
                 R = np.array([[np.cos(theta), -np.sin(theta)], 
@@ -106,46 +186,35 @@ class InstanceMatcher:
               
                 v_i_transformed = ds * np.dot(R, v_i)
                 
-                barycenter_j = (p_j[0] + v_i_transformed[0], p_j[1] + v_i_transformed[1])
-                aa.cast_vote(barycenter_j, (p_i, p_j))
+                barycenter_j = (
+                    p_j[0] + v_i_transformed[0], 
+                    p_j[1] + v_i_transformed[1]
+                )
+                
+                aa.cast_vote(i, barycenter_j, (p_i, p_j))
             
-            aa.nms()
-            if aa.check_votes():
-                found.append((aa, filename, model_img_shape))
+        if show_accumulator:
+            pass
+            # InstanceMatcher.display_accumulator(aa)
 
-            if show_accumulator:
-                fig, axes = plt.subplots(1, 2)
-                axes[0].imshow(cv.imread(filename)[:,:,::-1])
-                axes[0].set_axis_off()
-                axes[1].imshow(aa.arr, cmap='jet', interpolation='nearest')
-                axes[1].set_axis_off()
-                for i in range(aa.arr.shape[0]):
-                    for j in range(aa.arr.shape[1]):
-                        axes[1].text(j, i, f"{aa.arr[i, j]:.0f}", ha="center", va="center", color="w")
-                plt.suptitle(filename)
-                plt.tight_layout()
-                plt.show()
-        
-        if th == None:
-            try:
-                #th = np.mean([aa.get_votes() for aa, _, _ in found])
-                th = np.mean(np.concatenate([aa.get_votes() for aa, _, _ in found]))
-            except:
-                return [], []
+        aa.nms_3d(size=5, min_votes=th)
+        return self._extract_bboxes(aa)
+    
+    @staticmethod
+    def display_accumulator(accumulator, filename):
+        pass
+        # _, axes = plt.subplots(1, 2)
+        # axes[0].imshow(cv.imread(filename)[:,:,::-1])
+        # axes[0].set_axis_off()
+        # axes[1].imshow(accumulator.arr, cmap='jet', interpolation='nearest')
+        # axes[1].set_axis_off()
+        # for i in range(accumulator.arr.shape[0]):
+        #     for j in range(accumulator.arr.shape[1]):
+        #         axes[1].text(j, i, f"{accumulator[i, j]:.0f}", ha="center", va="center", color="w")
+        # plt.suptitle(filename)
+        # plt.tight_layout()
+        # plt.show()
 
-        instances = []
-        boxes = []
-        for aa, filename, model_img_shape in found:
-            for model_pts, target_pts in aa.get_matching_pts(th):
-                                
-                model_pts = np.float32(model_pts).reshape((-1,1,2))       
-                target_pts = np.float32(target_pts).reshape((-1,1,2))
-                H, _ = cv.findHomography(model_pts, target_pts, cv.RANSAC, 5.0)
-                
-                pts = np.float32([[0, 0],[model_img_shape[1]-1, model_img_shape[0]-1]]).reshape(-1,1,2)
-                transformed_pts = cv.perspectiveTransform(pts, H).reshape(-1, 2)
-                
-                instances.append(filename)
-                boxes.append(BoundingBox(transformed_pts))
-
-        return instances, boxes
+    @staticmethod
+    def display_models():
+        pass
